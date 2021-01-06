44b8f03d4bd17c8358032dbc17aa694740bc3914
honkling <62254220+honkling@users.noreply.github.com>
2021-01-05 23:26:24 -0600

    Add files via upload

---
+++ src/main/java/me/honkling/skriptutility/elements/conditions/CondPluginLoaded.java
@@ -0,0 +1,39 @@
+package me.honkling.skriptutility.elements.conditions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Condition;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.util.Kleenean;
+import jdk.internal.jline.internal.Nullable;
+import org.bukkit.event.Event;
+import org.bukkit.plugin.Plugin;
+
+public class CondPluginLoaded extends Condition {
+
+    static {
+        Skript.registerCondition(CondPluginLoaded.class, "%plugins% (1¦(is|are)|2¦(is(n't| not)|are(n't| not)) unloaded");
+    }
+
+    Expression<Plugin> plugin;
+
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parser) {
+        this.plugin = (Expression<Plugin>) exprs[0];
+        setNegated(parser.mark == 1);
+        return true;
+    }
+
+    @Override
+    public String toString(@Nullable Event event, boolean debug) {
+        return "Plugin is loaded- Plugin: " + plugin.toString(event, debug);
+    }
+
+    @Override
+    public boolean check(Event event) {
+        Plugin p = plugin.getSingle(event);
+        if(p == null) return isNegated();
+        return p.isEnabled() ? isNegated() : !isNegated();
+    }
+
+}
---
+++ src/main/java/me/honkling/skriptutility/elements/events/EvtPluginDisable.java
@@ -0,0 +1,46 @@
+package me.honkling.skriptutility.elements.events;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Literal;
+import ch.njol.skript.lang.SkriptEvent;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.util.Checker;
+import jdk.internal.jline.internal.Nullable;
+import org.bukkit.event.Event;
+import org.bukkit.event.server.PluginDisableEvent;
+import org.bukkit.plugin.Plugin;
+
+public class EvtPluginDisable extends SkriptEvent {
+
+    static {
+        Skript.registerEvent("On Plugin Disable", EvtPluginDisable.class, PluginDisableEvent.class, "[on] [plugin] disable [of %plugin%]");
+    }
+
+    Literal<Plugin> plugins;
+
+    @Override
+    public boolean init(Literal<?>[] args, int matchedPattern, ParseResult parser) {
+        plugins = (Literal<Plugin>) args[0];
+        return true;
+    }
+
+    @Override
+    public boolean check(Event e) {
+        if(plugins != null) {
+            Plugin plugin = ((PluginDisableEvent)e).getPlugin();
+            return plugins.check(e, new Checker<Plugin>() {
+                @Override
+                public boolean check(Plugin data) {
+                    return data == plugin;
+                }
+            });
+        }
+        return true;
+    }
+
+    @Override
+    public String toString(@Nullable Event e, boolean debug) {
+        return "Plugin disable event " + plugins.toString(e, debug);
+    }
+
+}
